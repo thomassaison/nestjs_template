a30acad8d8f3d81603472d3185179e54f0033998
Thomas Saison <tsaison9@gmail.com>
2022-01-27 00:49:45 +0100

    [config] Load application configuration in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import IApplicationConfig from './config/interfaces/application-config.interface';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const configService = app.get(ConfigService);
+    const applicationConfig =
+        configService.get<IApplicationConfig>('application');
+
+    await app.listen(applicationConfig.port);
 }
 
 bootstrap();
