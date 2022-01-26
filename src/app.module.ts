3ace39d5d22d9a62806f3a7cd7952cc3b94ce73d
Thomas Saison <tsaison9@gmail.com>
2022-01-27 00:39:52 +0100

    [AppModule] clean imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ApiModule } from './api/api.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApiModule } from './api/api.module';
-import { CoreModule } from './core/core.module';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
 import applicationConfig from './config/application.config';
+import { CoreModule } from './core/core.module';
 
 @Module({
     imports: [
