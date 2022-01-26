28f77e3a313699440e21f29e4292e7816bd1f4aa
Thomas Saison <tsaison9@gmail.com>
2022-01-27 00:35:25 +0100

    [config] Import config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
 import { CoreModule } from './core/core.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [ApiModule, CoreModule],
+    imports: [
+        ApiModule,
+        CoreModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
