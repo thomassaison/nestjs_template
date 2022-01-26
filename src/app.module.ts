6deabdca920ff064f8fc231dabc25c9cd4aa6385
Thomas Saison <tsaison9@gmail.com>
2022-01-27 00:30:45 +0100

    [archi] create core & api modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiModule } from './api/api.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-    imports: [],
+    imports: [ApiModule, CoreModule],
     controllers: [AppController],
     providers: [AppService],
 })
